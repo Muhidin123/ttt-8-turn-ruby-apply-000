cdd59a6b7afc14872247d40b9c0a15a640e5ad0e
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-08-25 21:30:16 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,56 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index)  == true
+    move(board, index)
+    display_board(board)
+  end
+  input_to_index(user_input)
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, ix)
+  if position_taken?(board, ix) == false && move_val?(ix) == true
+    return true
+  else
+    false
+  end
+end 
+
+
+
+def position_taken?(board, ix)
+  if board[ix] == " " || board[ix] == "" || board[ix] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def move_val?(num)
+  if num.between?(0, 8) == true
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, input_to_index, x = "X")
+  board[input_to_index] = x
+  return board
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  index -= 1
+  return index
+end
+
